af0c8e5aa9c748b0c8c4c3150e227eb9fef8a8b0
bang.nguyen2 <bangca85@gmail.com>
2024-08-16 08:32:13 +0700

    add find_most_frequent2

--- basic/buoi18/bai14.py
+++ basic/buoi18/bai14.py
@@ -24,6 +24,25 @@ def find_most_frequent(numbers):
     most_frequent = max(frequency, key=frequency.get)
     return most_frequent
 
+def find_most_frequent2(numbers):
+    frequency = {}
+    
+    # Tạo từ điển lưu trữ tần suất xuất hiện của các số
+    for num in numbers:
+        frequency[num] = frequency.get(num, 0) + 1
+    
+    # Tìm phần tử có tần suất xuất hiện nhiều nhất
+    most_frequent = None
+    max_count = 0
+    
+    for num, count in frequency.items():
+        if count > max_count:
+            max_count = count
+            most_frequent = num
+    
+    return most_frequent
+
+
 numbers = (1, 2, 3, 2, 4, 2, 5, 3)
 most_frequent = find_most_frequent(numbers)
 print(f"Phần tử xuất hiện nhiều nhất: {most_frequent}")
